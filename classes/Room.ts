93bdba98e7b9ba65c8f89909cdd2abdb0dd1791e
WalkingFrozenFish <chachin.ol@yandex.kz>
2022-11-01 20:54:06 +0600

    Сделал класс с массивом комнат, для передвижения по ним

--- classes/Room.ts
+++ classes/Room.ts
@@ -2,18 +2,82 @@ import { InventoryItem } from "./Inventory";
 
 export class Room {
     roomInventory: InventoryItem[] = []
+    roomId: number
+
+    constructor(roomId: number) {
+        this.roomId = roomId
+    }
 
     checkRoom = (): void => {
         console.log(this.roomInventory)
     }
 
-    putItemToInventory = (item: InventoryItem) => {
+    putToRoom = (item: InventoryItem) => {
         this.roomInventory.push(item)
     }
 
-    getItemFromInventory = (item: InventoryItem): InventoryItem => {
+    getFromRoom = (item: InventoryItem): InventoryItem => {
         return item
     }
-
-
 }
+
+
+class Rooms {
+    roomsArray: Room[] = []
+    currentRoom!: Room | null
+
+    constructor() {
+
+    }
+
+    changeRoom = (): void => {
+
+    }
+
+    showRooms = (): void => {
+
+    }
+}
+
+
+interface IEvent {
+    action(): void
+}
+
+class EventEnemy implements IEvent {
+    action = (): void => {
+        console.log("Enemy strategy")
+    }
+}
+
+class EventShop implements IEvent {
+    action = (): void => {
+        console.log("Shop strategy")
+    }
+}
+
+class EventChest implements IEvent {
+    action = (): void => {
+        console.log("Event chest")
+    }
+}
+
+class RoomEvent {
+    roomEvent: IEvent | undefined
+
+    setRoomEvent = (ev: IEvent): void => {
+        this.roomEvent = ev
+    }
+
+    action = (): void => {
+        this.roomEvent!.action()
+    }
+}
+
+const room = new RoomEvent()
+room.setRoomEvent(new EventChest())
+room.action()
+room.setRoomEvent(new EventEnemy())
+room.action()
+room.setRoomEvent(new EventShop())
+room.action()
